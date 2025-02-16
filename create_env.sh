1cbeac2877a565e740c7adf3db0a74f2b53037f6
ceastld <1950946176@qq.com>
2025-02-16 07:08:55 +0000

    Add project setup files and environment configuration

---
+++ create_env.sh
@@ -0,0 +1,5 @@
+ml CUDA/11.6.0
+conda env create -f environment.yml
+
+conda activate dinet
+gdown https://drive.google.com/uc?id=1CkeEn7l3PuubuJIMWNjWpIrt0HDd_AB3
---
+++ run.sh
@@ -0,0 +1,3 @@
+ml CUDA/11.6.0
+
+python inference.py --mouth_region_size=256 --source_video_path=./asserts/examples/test1.mp4 --source_openface_landmark_path=./asserts/examples/testxxx.csv --driving_audio_path=./asserts/examples/driving_audio_1.wav --pretrained_clip_DINet_path=./asserts/clip_training_DINet_256mouth.pth  
